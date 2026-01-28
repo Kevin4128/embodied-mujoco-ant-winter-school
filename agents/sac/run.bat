5652dc727f5600819763ea48c4caa3345c3c65c9
Kevin4128 <88260749+Kevin4128@users.noreply.github.com>
2026-01-28 14:25:14 +0800

    bash version of sac run.sh

---
+++ agents/sac/run.bat
@@ -0,0 +1,31 @@
+@echo off
+
+REM Learn on simulation
+if "%1"=="sim" (
+    python sac_cleanrl.py ^
+        --render_mode rgb_array ^
+        --dt 0.12 ^
+        --env_id SimEmbodiedAnt ^
+        --learning_starts 2000 ^
+        --batch_size 256 ^
+        --task_type forward ^
+        --model_path ../../sim/assets/embodied_mujoco_ant_higher_fidelity.xml ^
+        --capture_video ^
+        --exp_name sim_learning_to_walk
+)
+
+REM Learn on hardware
+if "%1"=="hw" (
+    python sac_cleanrl.py ^
+        --render_mode rgb_array ^
+        --dt 0.12 ^
+        --seed 1 ^
+        --env_id HwEmbodiedAnt ^
+        --hw_config ../../embodied_ant_env/ant1.json ^
+        --learning_starts 2000 ^
+        --task_type forward ^
+        --exp_name ant_forward ^
+        --weights_path runs/sim_learning_to_walk_with_video_forward_20260122-112812_seed_1/weights_and_args/ ^
+        --eval True
+    REM --capture_video
+)
